5f2066d71d7d344e3e7276a5d89bff9e12aeb469
AkshayRanaGujjAr <akshaybhati28@gmail.com>
2017-08-15 14:43:32 +0530

    add modal and button

--- client/main.js
+++ client/main.js
@@ -1,12 +1,25 @@
 import { Template } from 'meteor/templating';
+import {Notes} from '../lib/collection.js';
 
 import './main.html';
 
 Template.body.helpers({
-  notes:[
-    {text:"akshay"},
-    {text:"aditya"},
-    {text:"yash"},
-    {text:"abhishek"}
-  ]
+  // notes:[
+  //   {text:"akshay"},
+  //   {text:"aditya"},
+  //   {text:"yash"},
+  //   {text:"abhishek"}
+  // ]
+
+
+  notes(){
+    return Notes.find();
+  }
+});
+Template.add.events({
+  'submit .add-form':function(){
+    console.log("123");
+
+    return false;
+  }
 });
